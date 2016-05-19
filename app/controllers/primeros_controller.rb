e06b85541258b120a90075ac4752f264eeea9c20
abel <abel@Ozymandias.Home>
2016-05-19 16:16:19 -0400

    - Se importa la librería para realizar el análisis de métricas. - Se realiza el análisis de los archivos .php con la herramienta PHPDepend para obtener las métricas a partir del código. - Se almacenan las métricas calculadas en una variable para mostrarlas en la vista.

--- app/controllers/primeros_controller.rb
+++ app/controllers/primeros_controller.rb
@@ -1,6 +1,7 @@
 class PrimerosController < ApplicationController
   before_action :set_primero, only: [:show, :edit, :update, :destroy]
   require 'MVC-PHP'
+  require 'PHPDependEngine'
   #variables para controlar elementos de un controlador y un modelo
   $elementsToAnalizer = ["PDO_METHODS","PDO_STATEMENT","GET","POST","DBA_STATEMENT"]
   $elementsOfModel = ["PDO_METHODS","PDO_STATEMENT","DBA_STATEMENT"]
@@ -32,7 +33,7 @@ class PrimerosController < ApplicationController
     @primero = Primero.new
     @files = []
     @rows = []
-
+    @metrics = []
     #@analyzer = Primero.new(analyzer_params)
     #puts YAML::dump(params[:primero][:scripts])
     #puts YAML::dump(params[:images])
@@ -42,6 +43,7 @@ class PrimerosController < ApplicationController
           #@fichero = script.read
           ex_file = script.content_type.split("/")
           file = script.read
+          file_path = script.tempfile.path
           file_name = script.original_filename
           script = script.open()
           script.each do | lines |
@@ -49,16 +51,22 @@ class PrimerosController < ApplicationController
           end
           #puts ex_file[1]
           #puts file
+          if (ex_file[1] == "x-php")
+            @metrics_analyzer = MetricsAnalyzer.new
+            @metrics.push(@metrics_analyzer.analyze_metrics(file_path))
+          end
           if (ex_file[1] == "x-php") || (ex_file[1] == "html")
             sections = MvcPhp.new.getSections(file.downcase,@rows,file_name)
             @files.push(sections)
           else
+            #DUDA: que pasa si redirige con esta acción pero en medio en un each?. Según rails, redirect ni render terminan la acción actual (create) por lo que no terminaría el each.
             flash[:notice] = 'El archivo que se ha ingresado esta vacio.'
             redirect_to :back
           end
           @rows = []
         }
     end
+    @metrics.flatten(1)
     $lexical_analyzer = @files
     respond_to do |format|
       if @files.nil?
@@ -98,7 +106,7 @@ class PrimerosController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_primero
@@ -108,5 +116,12 @@ class PrimerosController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def primero_params
       params.require(:primero).permit(:cadena, :resultado)
+    end
+    
+    def get_metrics(file_path='')
+      # TO DO: comprobar de alguna forma que la librería está disponible. UPDATE: necesario?
+      # TO DO: retornar una cadena vacía en caso que la librería no se haya cargado bien 
+      metrics = MetricAnalyzer.new.analyze_metrics(file_path)
+      return metrics
     end
 end
