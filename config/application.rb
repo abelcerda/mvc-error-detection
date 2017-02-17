de3d958a564457d6018c5c7f1803359f7048a44c
abel <abel@Ozymandias.Home>
2017-02-16 20:59:31 -0500

    Se quita la dependencia de la aplicación a una base de datos. La aplicación ahora puede correr sin la necesidad de una base de datos ya que no la utiliza.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all' # No se usa todo porque incluye ActiveRecord pero la aplicación no usará base de datos
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,6 +26,6 @@ module Tesis
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
